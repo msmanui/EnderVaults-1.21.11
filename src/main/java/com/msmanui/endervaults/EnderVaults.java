53a60fe41e9fd1d29abf1562729ee320cb4c7cc3
msmanui <63617395+msmanui@users.noreply.github.com>
2026-03-18 16:49:04 -0300

    Add main plugin class for EnderVaults.

---
+++ src/main/java/com/msmanui/endervaults/EnderVaults.java
@@ -0,0 +1,15 @@
+package com.msmanui.endervaults;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class EnderVaults extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
